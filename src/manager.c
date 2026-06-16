53eaa3d49908c15342b190257c265af96b04f994
majdm25 <62832230+majdm25@users.noreply.github.com>
2026-06-16 22:05:41 +0300

    Update manager.c

--- src/manager.c
+++ src/manager.c
@@ -1,54 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <sys/wait.h>
 
 int main()
 {
-    pid_t cpu_pid;
-    pid_t mem_pid;
-    pid_t io_pid;
+    pid_t cpu_pid, mem_pid, io_pid;
 
-    printf("=================================\n");
-    printf("      OS Detective Lab\n");
-    printf("=================================\n");
-    printf("Manager PID: %d\n\n", getpid());
+    printf("Manager PID: %d\n", getpid());
 
     cpu_pid = fork();
-
     if(cpu_pid == 0)
-    {
-        execl("./worker_cpu", "./worker_cpu", NULL);
-        perror("execl");
-        exit(1);
-    }
+        execl("./worker_cpu", "worker_cpu", NULL);
 
     mem_pid = fork();
-
     if(mem_pid == 0)
-    {
-        execl("./worker_memory", "./worker_memory", NULL);
-        perror("execl");
-        exit(1);
-    }
+        execl("./worker_memory", "worker_memory", NULL);
 
     io_pid = fork();
-
     if(io_pid == 0)
+        execl("./worker_io", "worker_io", NULL);
+
+    while(1)
     {
-        execl("./worker_io", "./worker_io", NULL);
-        perror("execl");
-        exit(1);
+        sleep(1);
     }
-
-    printf("CPU Worker PID    : %d\n", cpu_pid);
-    printf("Memory Worker PID : %d\n", mem_pid);
-    printf("IO Worker PID     : %d\n", io_pid);
-while(1)
-{
-    for(int i = 0; i < 100000000; i++) {
-        int x = i * i;
-    }
-}
-
-    return 0;
 }
