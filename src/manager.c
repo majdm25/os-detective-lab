f73def4c897eae3b02e7cbeefbbe170e5659c0d4
majdm25 <62832230+majdm25@users.noreply.github.com>
2026-06-16 22:22:04 +0300

    Update manager.c

--- src/manager.c
+++ src/manager.c
@@ -10,17 +10,33 @@ int main()
     printf("Manager PID: %d\n", getpid());
 
     cpu_pid = fork();
+
     if(cpu_pid == 0)
+    {
         execl("./worker_cpu", "worker_cpu", NULL);
+        perror("execl cpu");
+        exit(1);
+    }
 
     mem_pid = fork();
+
     if(mem_pid == 0)
+    {
         execl("./worker_memory", "worker_memory", NULL);
+        perror("execl memory");
+        exit(1);
+    }
 
     io_pid = fork();
-    if(io_pid == 0)
-        execl("./worker_io", "worker_io", NULL);
 
+    if(io_pid == 0)
+    {
+        execl("./worker_io", "worker_io", NULL);
+        perror("execl io");
+        exit(1);
+    }
+
+    
     while(1)
     {
         sleep(1);
