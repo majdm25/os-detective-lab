110ae88fdd871d127ac4c51c490c605140ebf506
majdm25 <62832230+majdm25@users.noreply.github.com>
2026-06-16 19:43:39 +0300

    Create manager.c

---
+++ src/manager.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main()
+{
+    pid_t cpu_pid;
+    pid_t mem_pid;
+    pid_t io_pid;
+
+    printf("=================================\n");
+    printf("      OS Detective Lab\n");
+    printf("=================================\n");
+    printf("Manager PID: %d\n\n", getpid());
+
+    cpu_pid = fork();
+
+    if(cpu_pid == 0)
+    {
+        execl("./worker_cpu", "./worker_cpu", NULL);
+        perror("execl");
+        exit(1);
+    }
+
+    mem_pid = fork();
+
+    if(mem_pid == 0)
+    {
+        execl("./worker_memory", "./worker_memory", NULL);
+        perror("execl");
+        exit(1);
+    }
+
+    io_pid = fork();
+
+    if(io_pid == 0)
+    {
+        execl("./worker_io", "./worker_io", NULL);
+        perror("execl");
+        exit(1);
+    }
+
+    printf("CPU Worker PID    : %d\n", cpu_pid);
+    printf("Memory Worker PID : %d\n", mem_pid);
+    printf("IO Worker PID     : %d\n", io_pid);
+
+    while(1)
+    {
+        sleep(5);
+    }
+
+    return 0;
+}
